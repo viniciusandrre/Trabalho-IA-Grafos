0cbab9228b9ff396ed18548f2dd224086e8e0366
vinciusandrre <viniciusandre_pereira@outlook.com>
2024-12-14 14:44:01 -0300

    Criação da Função h_score

--- Trabalho Grafos/Grafo.py
+++ Trabalho Grafos/Grafo.py
@@ -1,5 +1,11 @@
 from pyamaze import maze, agent
 
+def h_score(celula,destino):
+    linhac = celula[0]
+    colunac = celula[1]
+    linhad = destino[0]
+    return abs(colunac - colunad) + abs(linhac - linhad)
+
 labirinto = maze();
 labirinto.CreateMaze()
 
